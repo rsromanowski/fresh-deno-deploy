b3ec4e2abe4ac27f96ddeb4e49b9577d386edc11
rsromanowski <79664656+rromanowski-figure@users.noreply.github.com>
2023-03-17 09:22:01 -0400

    update League interface

--- islands/Search.tsx
+++ islands/Search.tsx
@@ -11,19 +11,19 @@ interface User {
 }
 
 interface League {
-  "total_rosters": 12;
-  "status": "pre_draft"; // can also be "drafting", "in_season", or "complete"
-  "sport": "nfl";
+  "total_rosters": number;
+  "status": string; // "pre_draft"; // can also be "drafting", "in_season", or "complete"
+  "sport": string; // nfl
   // "settings": { settings object },
-  "season_type": "regular";
-  "season": "2018";
+  "season_type": string; // "regular";
+  "season": string; // "2018";
   // "scoring_settings": { scoring_settings object },
   // "roster_positions": [ roster positions array ],
-  "previous_league_id": "198946952535085056";
-  "name": "Sleeperbot Friends League";
-  "league_id": "289646328504385536";
-  "draft_id": "289646328508579840";
-  "avatar": "efaefa889ae24046a53265a3c71b8b64";
+  "previous_league_id": string; // "198946952535085056";
+  "name": string; // "Sleeperbot Friends League";
+  "league_id": string; // "289646328504385536";
+  "draft_id": string; // "289646328508579840";
+  "avatar": string; // "efaefa889ae24046a53265a3c71b8b64";
 }
 
 interface LeagueUser {
